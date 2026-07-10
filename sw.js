833bc1cc35428d10798bf1db1a27f94a51e37cd3
risaleo <iloveleo7@gmail.com>
2026-07-10 20:18:19 +0900

    15.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aircon-master-v15-1';
+const CACHE='aircon-hanbai-master-v15-design-2';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
