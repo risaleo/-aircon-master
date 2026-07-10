f62a7767d3e46637cf052de1cf1fd713e8dc9c0c
risaleo <iloveleo7@gmail.com>
2026-07-11 06:36:09 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = 'aircon-hanbai-master-v205';
+const CACHE = 'aircon-hanbai-master-v206';
 const ASSETS = [
   './',
-  './index.html?v=205',
-  './manifest.json?v=205',
-  './icon.svg?v=205'
+  './index.html?v=206',
+  './manifest.json?v=206',
+  './icon.svg?v=206'
 ];
 
 self.addEventListener('install', event => {
@@ -27,10 +27,10 @@ self.addEventListener('fetch', event => {
       fetch(event.request, {cache:'no-store'})
         .then(response => {
           const copy = response.clone();
-          caches.open(CACHE).then(cache => cache.put('./index.html?v=205', copy));
+          caches.open(CACHE).then(cache => cache.put('./index.html?v=206', copy));
           return response;
         })
-        .catch(() => caches.match('./index.html?v=205'))
+        .catch(() => caches.match('./index.html?v=206'))
     );
     return;
   }
