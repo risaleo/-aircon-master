28b4fd723662596dc655a9a5a449c333ffc07435
risaleo <iloveleo7@gmail.com>
2026-07-10 20:21:52 +0900

    15.1

--- sw.js
+++ sw.js
@@ -1,5 +1,47 @@
-const CACHE='aircon-hanbai-master-v15-design-2';
-const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = 'aircon-hanbai-master-v151';
+const ASSETS = [
+  './',
+  './index.html?v=151',
+  './manifest.json?v=151',
+  './icon.svg?v=151'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request, {cache:'no-store'})
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put('./index.html?v=151', copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html?v=151'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request, {cache:'no-store'})
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
