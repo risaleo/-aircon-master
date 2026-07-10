3b1cb12e0855ecb72805bd53252c57dce3ae1631
risaleo <iloveleo7@gmail.com>
2026-07-10 20:09:46 +0900

    15

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='aircon-master-v14-1';
-const ASSETS=['./','./index.html','./style.css?v=141','./script.js?v=141','./manifest.webmanifest?v=141','./apple-touch-icon.png?v=141','./icon-192.png?v=141','./icon-512.png?v=141'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
+const CACHE='aircon-master-v15-1';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
