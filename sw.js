1666cce175fcfaf85deb2670cf7363ed33eb1c97
risaleo <iloveleo7@gmail.com>
2026-07-11 21:37:58 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,53 +1,5 @@
-const CACHE='aircon-hanbai-master-v230';
-const ASSETS=['./apple-touch-icon.png?v=230','./favicon-32.png?v=230',
-  './',
-  './index.html?v=230',
-  './manifest.json?v=230',
-  './version.json?v=230',
-  './icon-180.png?v=230',
-  './icon-192.png?v=230',
-  './icon-512.png?v=230',
-  './icon-1024.png?v=230'
-];
-
-self.addEventListener('install',event=>{
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
-});
-
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
-      .then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener('message',event=>{
-  if(event.data && event.data.type==='SKIP_WAITING')self.skipWaiting();
-});
-
-self.addEventListener('fetch',event=>{
-  if(event.request.mode==='navigate'){
-    event.respondWith(
-      fetch(event.request,{cache:'no-store'})
-        .then(response=>{
-          const copy=response.clone();
-          caches.open(CACHE).then(cache=>cache.put('./index.html?v=230',copy));
-          return response;
-        })
-        .catch(()=>caches.match('./index.html?v=230'))
-    );
-    return;
-  }
-
-  event.respondWith(
-    fetch(event.request,{cache:'no-store'})
-      .then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-        return response;
-      })
-      .catch(()=>caches.match(event.request))
-  );
-});
+const CACHE='aircon-master-v300';
+const ASSETS=['./','./index.html?v=300','./manifest.json','./hero-aircon.jpg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html?v=300')))));
