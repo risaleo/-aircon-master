ff527bb62e6acd15ccbae87ca758edaa85a1ed44
risaleo <iloveleo7@gmail.com>
2026-07-11 20:21:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE='aircon-hanbai-master-v220';
-const ASSETS=['./apple-touch-icon.png?v=220','./favicon-32.png?v=220',
+const CACHE='aircon-hanbai-master-v221';
+const ASSETS=['./apple-touch-icon.png?v=221','./favicon-32.png?v=221',
   './',
-  './index.html?v=220',
-  './manifest.json?v=220',
-  './version.json?v=220',
-  './icon-180.png?v=220',
-  './icon-192.png?v=220',
-  './icon-512.png?v=220',
-  './icon-1024.png?v=220'
+  './index.html?v=221',
+  './manifest.json?v=221',
+  './version.json?v=221',
+  './icon-180.png?v=221',
+  './icon-192.png?v=221',
+  './icon-512.png?v=221',
+  './icon-1024.png?v=221'
 ];
 
 self.addEventListener('install',event=>{
@@ -33,10 +33,10 @@ self.addEventListener('fetch',event=>{
       fetch(event.request,{cache:'no-store'})
         .then(response=>{
           const copy=response.clone();
-          caches.open(CACHE).then(cache=>cache.put('./index.html?v=220',copy));
+          caches.open(CACHE).then(cache=>cache.put('./index.html?v=221',copy));
           return response;
         })
-        .catch(()=>caches.match('./index.html?v=220'))
+        .catch(()=>caches.match('./index.html?v=221'))
     );
     return;
   }
